84b223f85cab28044135a5c89b39cd7b888bd8ba
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-11-22 13:25:35 +0200

    add MovieList

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from 'react';
 import fetchTrandingMovies from 'components/Api/Api';
-import { NavLink } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import MoviesList from 'components/MoviesList/MoviesList'
 
 const Home = () => {
-  //   const [home, setHome] = useState([]);
   const [trendingMovies, setTrendingMovies] = useState([]);
 
   useEffect(() => {
-    // axios.get(setHome)
     async function getTrandingMovies() {
       try {
         const movies = await fetchTrandingMovies();
-        console.log(movies);
+        console.log(movies.results);
         setTrendingMovies(prevState => [...prevState, ...movies.results]);
+        // console.log(trendingMovies)
+
       } catch (error) {
         console.log(error);
       }
@@ -22,19 +21,6 @@ const Home = () => {
     getTrandingMovies();
   }, []);
 
-  const MoviesList = ({ trendingMovies }) => {
-    const location = useLocation();
-    if (!trendingMovies) return;
-    return (
-      <ul>
-        {trendingMovies.map(movie => (
-          <li key={movie.id}>
-            <NavLink to={`/movies/${movie.id}`} state={{ from: location }} />
-          </li>
-        ))}
-      </ul>
-    );
-  };
 
   return (
     <>
