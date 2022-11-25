6b9930f130b4297a053234d1c5bfa9fa9a48e0e1
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-11-25 16:13:05 +0200

    fix input

--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -10,37 +10,6 @@ const SearchMovie = ({ onChange }) => {
   );
 };
 
-// const SearchMovie = ({ onSubmit }) => {
-//   const {
-//     register,
-//     handleSubmit,
-//     reset,
-//     formState: { errors },
-//   } = useForm({ defaultValues: { inputMovie: '' } });
-
-//   const formHandleSubmit = ({ inputMovie }) => {
-//     onSubmit(inputMovie);
-//     reset();
-//   };
-
-//   return (
-//     <div>
-//     {/* <Box position="relative" display="inline-block"> */}
-//       <Form autoComplete="off" onSubmit={handleSubmit(formHandleSubmit)}>
-//         <Input
-//           type="text"
-//           {...register('inputMovie', { required: 'This field is required!' })}
-//           autoFocus
-//         />
-//         {/* <SearchButton type="submit">
-//           <MdMovieFilter size={40} />
-//         </SearchButton> */}
-//       </Form>
-//       <ErrorMessage>{errors.inputMovie?.message}</ErrorMessage>
-//     {/* </Box> */}
-//     </div>
-//   );
-// };
 
 export default SearchMovie;
 
